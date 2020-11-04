60992716decb30122dd17f24c1e3332e04aa35a9
Alison Appling <aappling@usgs.gov>
2020-11-04 11:29:33 -0500

    copy_zip_repo is dangerous but i need a backup

--- src/file_functions.R
+++ src/file_functions.R
@@ -1,5 +1,5 @@
 zip_this <- function(out_file, .object){
-  
+
   if ('data.frame' %in% class(.object)){
     filepath <- basename(out_file) %>% tools::file_path_sans_ext() %>% paste0('.csv') %>% file.path(tempdir(), .)
     write_csv(.object, path = filepath)
@@ -18,11 +18,45 @@ zip_this <- function(out_file, .object){
 zip_obs <- function(out_file, in_file){
   if (grepl('csv', in_file)) {
     zip_this(out_file, .object = readr::read_csv(in_file))
-    
+
   } else if (grepl('rds', in_file)) {
     zip_this(out_file, .object = readRDS(in_file))
   } else {
     message('There is no reader for this filetype. Please modify function zip_obs.')
   }
-  
+
+}
+
+zip_dir <- function(out_file, in_dir) {
+  curdir <- getwd()
+  on.exit(setwd(curdir))
+  setwd(dirname(in_dir))
+  zip::zip(file.path(curdir, out_file), files = basename(.object))
+}
+
+copy_zip_repo <- function(out_file, cache_dir, source_dir) {
+  # cache_dir is probably created, but just in case...
+  if(!dir.exists(cache_dir)) dir.create(cache_dir)
+
+  # copy the repo into the same dir where we'll create the zip file
+  unlink(cache_dir, recursive=TRUE)
+  file.copy(source_dir, dirname(cache_dir), recursive=TRUE)
+
+  # remove all .gitignored files, .git/, and .gitignore
+  gitignore <- readr::read_lines(file.path(cache_dir, '.gitignore'))
+  for(gi in gitignore) {
+    gi_regex <- gi %>%
+      # gsub('\\.', '\\\\.', .) %>%
+      gsub('\\*\\*', '*', .) %>%
+      gsub('\\*', '.*', .) %>%
+      gsub('/$', '', .) %>%
+      file.path(cache_dir, .)
+    message('unlinking ', gi_regex)
+    # unlink(gi_regex, recursive=TRUE)
+  }
+  unlink(file.path(cache_dir, '.git'), recursive=TRUE)
+  unlink(file.path(cache_dir, '.gitignore'))
+
+  # zip the repo
+  zip_dir(out_file, cache_dir)
 }
