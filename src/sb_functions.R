00742f22dfef1e020dce6b0174a5e8df851361c3
Alison Appling <aappling@usgs.gov>
2020-11-06 10:19:06 -0500

    use locally-made FGDC template for customizability

--- src/sb_functions.R
+++ src/sb_functions.R
@@ -1,12 +1,12 @@
 sb_replace_files <- function(sb_id, ..., file_hash){
-  
+
   if (!sbtools::is_logged_in()){
 
     sb_secret <- dssecrets::get_dssecret("cidamanager-sb-srvc-acct")
     sbtools::authenticate_sb(username = sb_secret$username, password = sb_secret$password)
 
   }
-  
+
   hashed_filenames <- c()
   if (!missing(file_hash)){
     hashed_filenames <- yaml.load_file(file_hash) %>% names %>% sort() %>% rev()
@@ -18,17 +18,17 @@ sb_replace_files <- function(sb_id, ..., file_hash){
   if (length(files) > 0){
     item_replace_files(sb_id, files = files)
   }
-  
+
 }
 
-sb_render_post_xml <- function(sb_id, ..., xml_file = NULL){
-  
+sb_render_post_xml <- function(sb_id, ..., xml_file = NULL, template){
+
   if (is.null(xml_file)){
     xml_file <- file.path(tempdir(), paste0(sb_id,'.xml'))
   }
-  
+
   render(filename = xml_file, ...)
-  
+
   sb_replace_files(sb_id = sb_id, xml_file)
-  
+
 }
